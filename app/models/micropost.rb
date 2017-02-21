4cc41d14706bac8d2ebdbf21078be431c0ce03ad
bdnmnm <ivdmamsc57argstr@live.jp>
2017-02-22 01:00:16 +0900

    Add validation to Users and Microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,7 @@
 class Micropost < ApplicationRecord
+  # １つのマイクロポストは1人のユーザーにのみ属する
+  belongs_to :user
+  validates :content,
+    length: { maximum: 140 },
+    presence: true  #コンテンツの存在確認
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  # １人のユーザーに複数のマイクロポストがある
+  has_many :microposts
+  # Userモデルに存在性のバリデージョン
+  validates :name, presence: true
+  validates :email, presence: true
 end
